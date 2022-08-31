7e766778328709d93fc86eb03a8e05021c91c9bc
AgustinFredes98 <agustin.fredes@outlook.com.ar>
2022-08-31 05:54:29 -0300

    feat nextBuildUpdated for github pages

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,12 +1,22 @@
 import "../styles/styles.css"
-import Script from "next/script";
 
 function MyApp({ Component, pageProps }) {
   return (
-    <> 
-      <script>{"console.log(window.location.href);window.location.href = '/WebHipico/#' + window.location.pathname.replace('/WebHipico', ''"}
-      </script>
-      <script src="https://cdn.tailwindcss.com"></script>
+    <>
+      <script src="https://cdn.tailwindcss.com">{"amigo tu me oyes"}</script>
+      <style>
+        {`
+          html {
+            background-color: rgb(255, 136, 0);
+          }
+
+          body, html, #__next {
+            height: 100%;
+            width: 100%;
+            position: absolute
+          }
+        `}
+      </style>
       <Component {...pageProps} />
     </>
   )
