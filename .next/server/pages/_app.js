a8d140fa924c5df51be4c25dc470ec16c7efb86a
AgustinFredes98 <agustin.fredes@outlook.com.ar>
2022-08-31 05:55:26 -0300

    revertion of nextjs aptem

--- .next/server/pages/_app.js
+++ .next/server/pages/_app.js
@@ -1,3 +1,4 @@
+"use strict";
 (() => {
 var exports = {};
 exports.id = 888;
@@ -7,26 +8,33 @@ exports.modules = {
 /***/ 2654:
 /***/ ((__unused_webpack_module, __webpack_exports__, __webpack_require__) => {
 
-"use strict";
 __webpack_require__.r(__webpack_exports__);
 /* harmony export */ __webpack_require__.d(__webpack_exports__, {
 /* harmony export */   "default": () => (__WEBPACK_DEFAULT_EXPORT__)
 /* harmony export */ });
 /* harmony import */ var react_jsx_runtime__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(997);
 /* harmony import */ var react_jsx_runtime__WEBPACK_IMPORTED_MODULE_0___default = /*#__PURE__*/__webpack_require__.n(react_jsx_runtime__WEBPACK_IMPORTED_MODULE_0__);
-/* harmony import */ var next_script__WEBPACK_IMPORTED_MODULE_1__ = __webpack_require__(4298);
-/* harmony import */ var next_script__WEBPACK_IMPORTED_MODULE_1___default = /*#__PURE__*/__webpack_require__.n(next_script__WEBPACK_IMPORTED_MODULE_1__);
-
 
 
 function MyApp({ Component , pageProps  }) {
     return /*#__PURE__*/ (0,react_jsx_runtime__WEBPACK_IMPORTED_MODULE_0__.jsxs)(react_jsx_runtime__WEBPACK_IMPORTED_MODULE_0__.Fragment, {
         children: [
             /*#__PURE__*/ react_jsx_runtime__WEBPACK_IMPORTED_MODULE_0__.jsx("script", {
-                children: "console.log(window.location.href);window.location.href = '/WebHipico/#' + window.location.pathname.replace('/WebHipico', ''"
+                src: "https://cdn.tailwindcss.com",
+                children: "amigo tu me oyes"
             }),
-            /*#__PURE__*/ react_jsx_runtime__WEBPACK_IMPORTED_MODULE_0__.jsx("script", {
-                src: "https://cdn.tailwindcss.com"
+            /*#__PURE__*/ react_jsx_runtime__WEBPACK_IMPORTED_MODULE_0__.jsx("style", {
+                children: `
+          html {
+            background-color: rgb(255, 136, 0);
+          }
+
+          body, html, #__next {
+            height: 100%;
+            width: 100%;
+            position: absolute
+          }
+        `
             }),
             /*#__PURE__*/ react_jsx_runtime__WEBPACK_IMPORTED_MODULE_0__.jsx(Component, {
                 ...pageProps
@@ -39,34 +47,9 @@ function MyApp({ Component , pageProps  }) {
 
 /***/ }),
 
-/***/ 4298:
-/***/ ((module, __unused_webpack_exports, __webpack_require__) => {
-
-/* unused reexport */ __webpack_require__(699)
-
-
-/***/ }),
-
-/***/ 2796:
-/***/ ((module) => {
-
-"use strict";
-module.exports = require("next/dist/shared/lib/head-manager-context.js");
-
-/***/ }),
-
-/***/ 6689:
-/***/ ((module) => {
-
-"use strict";
-module.exports = require("react");
-
-/***/ }),
-
 /***/ 997:
 /***/ ((module) => {
 
-"use strict";
 module.exports = require("react/jsx-runtime");
 
 /***/ })
@@ -78,7 +61,7 @@ module.exports = require("react/jsx-runtime");
 var __webpack_require__ = require("../webpack-runtime.js");
 __webpack_require__.C(exports);
 var __webpack_exec__ = (moduleId) => (__webpack_require__(__webpack_require__.s = moduleId))
-var __webpack_exports__ = __webpack_require__.X(0, [699], () => (__webpack_exec__(2654)));
+var __webpack_exports__ = (__webpack_exec__(2654));
 module.exports = __webpack_exports__;
 
 })();
